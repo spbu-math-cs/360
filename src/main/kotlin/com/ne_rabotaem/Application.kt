71f0235995fb54cb1579d22df101166d48267288
Sid0r0vich <dmitriysidorcool@gmail.com>
2023-10-07 18:55:25 +0300

    feat: build server

---
+++ src/main/kotlin/com/ne_rabotaem/Application.kt
@@ -0,0 +1,16 @@
+package com.ne_rabotaem
+
+import com.ne_rabotaem.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.cio.*
+import io.ktor.server.engine.*
+
+fun main() {
+    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
---
+++ src/main/kotlin/com/ne_rabotaem/plugins/Serialization.kt
@@ -0,0 +1,11 @@
+package com.ne_rabotaem.plugins
+
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json()
+    }
+}
