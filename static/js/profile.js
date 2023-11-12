416411df5ef78a1a1349783baf5e4752469aad40
mozhaa <mozhay2005@gmail.com>
2023-11-12 13:10:42 +0300

    feat: design for invites, teams, in-team voting

--- static/js/profile.js
+++ static/js/profile.js
@@ -2,3 +2,68 @@ function logOut() {
     eraseCookie(TOKEN_COOKIE_NAME);
     window.location.href = "/";
 }
+
+function openPopup() {
+    $(".popup").addClass("active");
+}
+
+function closePopup() {
+    $(".popup").removeClass("active");
+}
+
+$(function() {
+    $("#id-input").inputFilter(function(value) {
+        return /^\d{0,4}$/.test(value);    // Allow digits only, using a RegExp
+    }, "Only digits allowed");
+
+    $("#id-input").on("input", function(e) {
+        if (this.value.length == 4) {
+            $(".search-bar").addClass("complete");
+        } else {
+            $(".search-bar").removeClass("complete");
+        }
+    })
+});
+
+// Restricts input for the set of matched elements to the given inputFilter function.
+// Source: https://stackoverflow.com/questions/995183/how-to-allow-only-numeric-0-9-in-html-inputbox-using-jquery/995193#995193
+(function($) {
+    $.fn.inputFilter = function(callback) {
+        return this.on("input keydown keyup mousedown mouseup select contextmenu drop focusout", function(e) {
+            if (callback(this.value)) {
+                // Accepted value
+                if (["keydown","mousedown","focusout"].indexOf(e.type) >= 0){
+                    $(this).removeClass("input-error");
+                    this.setCustomValidity("");
+                }
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+
+            } else if (this.hasOwnProperty("oldValue")) {
+                // Rejected value - restore the previous one
+                $(this).addClass("input-error");
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            } else {
+                // Rejected value - nothing to restore
+                this.value = "";
+            }
+        });
+    };
+}(jQuery));
+
+function resetInput() {
+    $("#id-input").val("");
+    $(".search-bar").removeClass("complete");
+}
+
+function inviteMember() {
+    if ($(".search-bar").hasClass("complete")) {
+        var inputId = $("#id-input").val();
+        console.log(inputId);
+        // TODO: send POST request
+        resetInput();
+        closePopup();
+    }
+}
--- static/js/vote.js
+++ static/js/vote.js
@@ -10,17 +10,22 @@ $(document).ready(function() {
     getTeamsOnDemo(parseInt(eventId));
 })
 
-function lockVoteCard(teamId, success) {
-    $(`#vote-card-team${teamId} :input`).prop("disabled", true);
-    $(`#vote-card-team${teamId}`).addClass("locked-vote-card");
-    $(`#grade-input-team${teamId}-btn`).addClass("hidden-vote-button");
-    var text = success ? "You have successfully voted." : "You've voted for this team before.";
-    var colorClass = success ? "text-success" : "text-primary";
-    $(`#grade-status-team${teamId}`)
-        .addClass("shown-grade-status")
-        .addClass(colorClass)
-        .removeClass("hidden-grade-status")
-        .text(text);
+function lockVoteCard(cardId, buttonId) {
+    $(`${cardId} input, ${cardId} textarea`).prop("disabled", true);
+    $(`${cardId}`).addClass("voted");
+    $(`${cardId} .vote-button`).hide();
+    $(`${cardId} .revote-button`).show();
+
+    $(`${buttonId}`).addClass("voted");
+}
+
+function unlockVoteCard(cardId, buttonId) {
+    $(`${cardId} input, ${cardId} textarea`).prop("disabled", false);
+    $(`${cardId}`).removeClass("voted");
+    $(`${cardId} .vote-button`).show();
+    $(`${cardId} .revote-button`).hide();
+
+    $(`${buttonId}`).removeClass("voted");
 }
 
 function getTeamsOnDemo(eventId) {
@@ -43,6 +48,42 @@ function getTeamsOnDemo(eventId) {
     });
 }
 
+function vote(teamId, eventId) {
+    var grade_1 = parseInt($(`#grade-input-team${teamId}-1`).val());
+    var grade_2 = parseInt($(`#grade-input-team${teamId}-2`).val());
+    var grade_3 = parseInt($(`#grade-input-team${teamId}-3`).val());
+    var grade_4 = parseInt($(`#grade-input-team${teamId}-4`).val());
+    var comment = $(`#grade-comment-team${teamId}`).val();
+
+    lockVoteCard(`#voting-card-team${teamId}`, `#voting-button-team${teamId}`);
+
+    // fetch('/demo/vote', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Accept': 'application/json',
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(
+    //         {
+    //             eventId: eventId,
+    //             teamId: teamId,
+    //             level: grade_1,
+    //             grade: grade_2,
+    //             presentation: grade_3,
+    //             additional: grade_4,
+    //             comment: comment
+    //         }
+    //     )
+    // })
+    // .then(response => {
+    //     if (response.ok) {
+    //         lockVoteCard(`#voting-card-team${teamId}`, `#voting-button-team${teamId}`);
+    //     } else {
+    //         response.text().then(text => alert(text));
+    //     }
+    // });
+}
+
 function addVoteCards(teams, eventId) {
     console.log(teams);
     teams.forEach(team => {
@@ -54,7 +95,7 @@ function addVoteCards(teams, eventId) {
 
         $("#voting-cards").append(
         `
-<form id="voting-card-team${teamId}" class="voting-card" onsubmit="voteSubmit(this)">
+<div id="voting-card-team${teamId}" class="voting-card card">
     <h1>Team ${teamNum}</h1>
     <h3>${projectName}</h3>
     <label for="grade-input-team${teamId}-1">Сложность спринта</label>
@@ -67,52 +108,32 @@ function addVoteCards(teams, eventId) {
     <input class="bonus-grade" type="range" id="grade-input-team${teamId}-4" min="0" max="3" value="0">
     <label for="grade-comment-team${teamId}">Комментарий</label>
     <textarea id="grade-comment-team${teamId}" rows="2"></textarea>
-    <button class="black-button" type="submit">Vote</button>
-</form>
+    <button class="vote-button black-button" type="button" onclick="vote(${teamId}, ${eventId})">Vote</button>
+    <button class="revote-button white-button" type="button" onclick="unlockVoteCard('#voting-card-team${teamId}', '#voting-button-team${teamId}')">Revote</button>
+</div>
         `
         );
 
-        $(`#vote-card-team${teamId}`).css({"opacity": "0", "z-index" : "0"});
+        $(`#upper-voting-buttons`).append(`
+            <div class="voting-button team-button" id="voting-button-team${teamId}" onclick="showCard('voting-card-team${teamId}', 'voting-button-team${teamId}')">${teamNum}</div>
+        `);
 
-        $(`#vote-card-team${teamId}`).submit(function(event) {
-            event.preventDefault();
-            var grade_1 = parseInt($(`#grade-input-team${teamId}-1`).val());
-            var grade_2 = parseInt($(`#grade-input-team${teamId}-2`).val());
-            var grade_3 = parseInt($(`#grade-input-team${teamId}-3`).val());
-            var grade_4 = parseInt($(`#grade-input-team${teamId}-4`).val());
-            var comment = $(`#grade-comment-team${teamId}`).val();
-
-            fetch('/demo/vote', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(
-                    {
-                        eventId: eventId,
-                        teamId: teamId,
-                        level: grade_1,
-                        grade: grade_2,
-                        presentation: grade_3,
-                        additional: grade_4,
-                        comment: comment
-                    }
-                )
-            })
-            .then(response => {
-                if (response.ok) {
-                    // lockVoteCard(teamId, true);
-                    console.log("success");
-                } else if (response.status == 409 /* HttpStatusCode.Conflict "You have already rated!" */){
-                    // lockVoteCard(teamId, false);
-                    console.log("already voted");
-                } else {
-                    response.text().then(text => alert(text));
-                }
-            });
-        });
+        $("#graph").append(`
+        <div class="item" id="graph-team${teamId}" style="--val: 0; --clr: #ffffff;">
+            <div class="label">${teamNum}</div>
+            <div class="value">0%</div>
+        </div>
+        `);
     });
+
+    $(`#upper-voting-buttons`).append(`
+        <div class="voting-button graphs-button" id="graphs-button" onclick="updateGraph();showCard('graphs-card', 'graphs-button')">
+            <img src="../img/chart.png" alt="Graphs">
+        </div>
+    `);
+
+    $(`.card`).css({"opacity": "0", "z-index" : "0"});
+    $(`.revote-button`).hide();
 
     $(`#voting-card-team1`).addClass("active");
     $(`#voting-button-team1`).addClass("active");
@@ -122,10 +143,7 @@ function addVoteCards(teams, eventId) {
 var selectedCardId = "voting-card-team1";
 var selectedButton = "voting-button-team1"; 
 
-function selectTeam(button) {
-    let cardId = $(button).attr("data-card-id");
-    let buttonId = $(button).attr("id");
-
+function showCard(cardId, buttonId) {
     $('#' + selectedCardId).removeClass("active");
     $('#' + cardId).addClass("active");
 
@@ -134,4 +152,52 @@ function selectTeam(button) {
 
     selectedCardId = cardId;
     selectedButton = buttonId;
+}
+
+function calcColorBetween(color1, color2, ratio) {
+    var hex = function(x) {
+        x = x.toString(16);
+        return (x.length == 1) ? '0' + x : x;
+    };
+
+    var r = Math.ceil(parseInt(color1.substring(0,2), 16) * ratio + parseInt(color2.substring(0,2), 16) * (1-ratio));
+    var g = Math.ceil(parseInt(color1.substring(2,4), 16) * ratio + parseInt(color2.substring(2,4), 16) * (1-ratio));
+    var b = Math.ceil(parseInt(color1.substring(4,6), 16) * ratio + parseInt(color2.substring(4,6), 16) * (1-ratio));
+
+    return hex(r) + hex(g) + hex(b);
+}
+
+MAX_GRADE = 20.0
+
+function calcGrade(teamId) {
+    var grade_1 = parseInt($(`#grade-input-team${teamId}-1`).val());
+    var grade_2 = parseInt($(`#grade-input-team${teamId}-2`).val());
+    var grade_3 = parseInt($(`#grade-input-team${teamId}-3`).val());
+    var grade_4 = parseInt($(`#grade-input-team${teamId}-4`).val());
+    return (grade_1 + grade_2 + grade_3) * (1.0 + grade_4 / 9.0);
+}
+
+function updateGraph() {
+    $(`.grades-graph > .item`).each(function() {
+        var teamId = parseInt($(this).attr("id").substring(10));
+        var grade = calcGrade(teamId);
+        var ratio = grade / MAX_GRADE;
+        var color = calcColorBetween("92DF7E", "12486B", ratio);
+        $(this).attr("style", `--val: ${ratio * 100}; --clr: #${color};`);
+        $(this).children(".value").html(`${Math.round(grade * 10) / 10}`);
+    });
+}
+
+function inTeamVote(eventId) {
+    var voteResult = [];
+    $(`#in-team-voting-card > input`).each(function() {
+        var grade = $(this).val();
+        var UID = $(this).attr("-data-UID");
+        voteResult.push({"UID": UID, "grade": grade});
+    });
+    
+    console.log(voteResult);
+    // TODO: send POST request
+
+    lockVoteCard(`#in-team-voting-card`, `#in-team-voting-button`);
 }
