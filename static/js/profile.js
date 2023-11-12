0c0f8fb0ea0c76909b7ede5eab746e901f4834f0
mozhaa <mozhay2005@gmail.com>
2023-11-12 11:07:17 +0300

    feat: green search bar if correct

--- static/js/profile.js
+++ static/js/profile.js
@@ -41,14 +41,22 @@ $(function() {
     });
 
     $("#id-input").inputFilter(function(value) {
-        return /^\d*$/.test(value);    // Allow digits only, using a RegExp
+        return /^\d{0,4}$/.test(value);    // Allow digits only, using a RegExp
     }, "Only digits allowed");
+
+    $("#id-input").on("input", function(e) {
+        if (this.value.length == 4) {
+            $(".search-bar").addClass("complete");
+        } else {
+            $(".search-bar").removeClass("complete");
+        }
+    })
 });
 
 // Restricts input for the set of matched elements to the given inputFilter function.
 // Source: https://stackoverflow.com/questions/995183/how-to-allow-only-numeric-0-9-in-html-inputbox-using-jquery/995193#995193
 (function($) {
-    $.fn.inputFilter = function(callback, errMsg) {
+    $.fn.inputFilter = function(callback) {
         return this.on("input keydown keyup mousedown mouseup select contextmenu drop focusout", function(e) {
             if (callback(this.value)) {
                 // Accepted value
@@ -59,11 +67,10 @@ $(function() {
                 this.oldValue = this.value;
                 this.oldSelectionStart = this.selectionStart;
                 this.oldSelectionEnd = this.selectionEnd;
+
             } else if (this.hasOwnProperty("oldValue")) {
                 // Rejected value - restore the previous one
                 $(this).addClass("input-error");
-                // this.setCustomValidity(errMsg);
-                // this.reportValidity();
                 this.value = this.oldValue;
                 this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
             } else {
