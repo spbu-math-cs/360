ee2bf7bfe89e3200cc78cdda66d40fc67956adaf
Sid0r0vich <dmitriysidorcool@gmail.com>
2023-11-25 13:44:12 +0300

    feat: load images!

--- src/main/kotlin/com/ne_rabotaem/features/demo/DemoRouting.kt
+++ src/main/kotlin/com/ne_rabotaem/features/demo/DemoRouting.kt
@@ -1,7 +1,5 @@
 package com.ne_rabotaem.features.demo
 
-import com.ne_rabotaem.database.grade.Demo_grade
-import com.ne_rabotaem.database.team.Team
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
@@ -13,26 +11,6 @@ fun Application.configureDemoRouting() {
 
         get("/demo_list") {
             DemoController(call).getDemos()
-        }
-
-        get("/demo/statistics") {
-            DemoController(call).getStatisticsPage()
-        }
-
-        get("/demo/statistics/average") {
-            DemoController(call).getTeamStatistics()
-        }
-
-        get("demo/statistics/comments") {
-            DemoController(call).getComments()
-        }
-
-        get("demo/statistics/average/all") {
-            DemoController(call).getAllTeamStatistics()
-        }
-
-        get("demo/statistics/comments/all") {
-            DemoController(call).getAllComments()
         }
     }
 }
