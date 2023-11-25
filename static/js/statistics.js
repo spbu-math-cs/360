ffefb5fac66534e46ab7f453704a7f1eb0454b59
mozhaa <mozhay2005@gmail.com>
2023-11-25 13:54:56 +0300

    feat: superuser statistics for past demos

--- static/js/statistics.js
+++ static/js/statistics.js
@@ -8,8 +8,133 @@ $(function() {
         return;
     }
 
-    fetchStatictics(eventId);
+    $(`#statistics-content > h1`).html(`Demo ${eventId}`);
+
+    // fetchRank(eventId);
+    fillPage({rank: "teacher"}, eventId);
 });
+
+function fetchRank(eventId) {
+    fetch(`/profile/info`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            response.json().then(responseJson => {
+                fillPage(responseJson, eventId);
+            })
+        } else {
+            response.text().then(text => function() { alert(text) });
+        }
+    });
+}
+
+function fillPage(userInfo, eventId) {
+    var rank = userInfo["rank"];
+    if (rank == "teacher") {
+        $(`#statistics-content`).append(`
+<div class="graphs-container">
+    <div class="graph-inner">
+        <h3>Сложность спринта</h3>
+        <div class="grades-graph" id="graph-1"></div>
+    </div>
+    <div class="graph-inner">
+        <h3>Уровень выполнения</h3>
+        <div class="grades-graph" id="graph-2"></div>
+    </div>
+    <div class="graph-inner">
+        <h3>Качество презентации</h3>
+    <div class="grades-graph" id="graph-3"></div>
+    </div>
+        <div class="graph-inner">
+        <h3>Дополнительные баллы</h3>
+        <div class="grades-graph" id="graph-4"></div>
+    </div>
+</div>
+        `);
+        fetchAllStatistics(eventId);
+    } else {
+        $(`#statistics-content`).append(`
+<div class="grades-container">
+    <div class="ratio-cell">
+        <div id="ratio-1" class="ratio">
+        <p>0</p>
+        </div>
+        <p>Сложность спринта</p>
+    </div>
+    <div class="ratio-cell">
+        <div id="ratio-2" class="ratio">
+        <p>0</p>
+        </div>
+        <p>Уровень выполнения</p>
+    </div>
+    <div class="ratio-cell">
+        <div id="ratio-3" class="ratio">
+        <p>0</p>
+        </div>
+        <p>Качество презентации</p>
+    </div>
+    <div class="ratio-cell">
+        <div id="ratio-4" class="ratio">
+        <p>0</p>
+        </div>
+        <p>Доп. баллы</p>
+    </div>
+</div>
+<h2>Comments</h2>
+<div id="comments-container"></div>
+        `);
+        fetchStatictics(eventId);
+    }
+}
+
+function fetchAllStatistics(eventId) {
+    fetch(`/demo/statistics/average/all?eventId=${eventId}`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            response.json().then(responseJson => {
+                setAllStatistics(responseJson, eventId);
+            })
+        } else {
+            response.text().then(text => function() {
+                alert("There's no statistics for this demo.");
+                window.location.href = "/demo";
+            });
+        }
+    });
+}
+
+function setAllStatistics(statistics, eventId) {
+    jQuery.each(statistics, function(teamId, grade) {
+        const criteria = ["avgLevel", "avgGrade", "avgPresentation", "avgAdditional"];
+        for (let i = 1; i < 5; i++) {
+            var criterion = criteria[i - 1];
+            if (criterion == "avgAdditional") {
+                var grade_1 = normalize(grade[criterion], 0, 3);
+            } else {
+                var grade_1 = normalize(grade[criterion], 1, 5);
+            }
+            var grade_100 = Math.floor(grade_1 * 100);
+            var color = calcColorBetween("92DF7E", "12486B", grade_1);
+            $(`#graph-${i}`).append(`
+    <div class="item" id="graph-${i}-team${teamId}" style="--val: ${grade_100}; --clr: #${color};">
+        <div class="label">${teamId}</div>
+        <div class="value">${Math.floor(grade[criterion] * 10) / 10}</div>
+    </div>
+            `);   
+        }
+    });
+}
 
 function fetchStatictics(eventId) {
     fetch(`/demo/statistics/average?eventId=${eventId}`, {
--- static/js/vote.js
+++ static/js/vote.js
@@ -373,40 +373,6 @@ function showCard(cardId, buttonId) {
     $('#' + buttonId).addClass("active");
 }
 
-function calcColorBetween(color1, color2, ratio) {
-    var hex = function(x) {
-        x = x.toString(16);
-        return (x.length == 1) ? '0' + x : x;
-    };
-
-    var r = Math.ceil(parseInt(color1.substring(0,2), 16) * ratio + parseInt(color2.substring(0,2), 16) * (1-ratio));
-    var g = Math.ceil(parseInt(color1.substring(2,4), 16) * ratio + parseInt(color2.substring(2,4), 16) * (1-ratio));
-    var b = Math.ceil(parseInt(color1.substring(4,6), 16) * ratio + parseInt(color2.substring(4,6), 16) * (1-ratio));
-
-    return hex(r) + hex(g) + hex(b);
-}
-
-MAX_GRADE = 20.0
-
-function calcGrade(teamId) {
-    var grade_1 = parseInt($(`#grade-input-team${teamId}-1`).val());
-    var grade_2 = parseInt($(`#grade-input-team${teamId}-2`).val());
-    var grade_3 = parseInt($(`#grade-input-team${teamId}-3`).val());
-    var grade_4 = parseInt($(`#grade-input-team${teamId}-4`).val());
-    return (grade_1 + grade_2 + grade_3) * (1.0 + grade_4 / 9.0);
-}
-
-function updateGraph() {
-    $(`.grades-graph > .item`).each(function() {
-        var teamId = parseInt($(this).attr("id").substring(10));
-        var grade = calcGrade(teamId);
-        var ratio = grade / MAX_GRADE;
-        var color = calcColorBetween("92DF7E", "12486B", ratio);
-        $(this).attr("style", `--val: ${ratio * 100}; --clr: #${color};`);
-        $(this).children(".value").html(`${Math.round(grade * 10) / 10}`);
-    });
-}
-
 function inTeamVote(eventId) {
     var voteResult = [];
     $(`#in-team-voting-card > input`).each(function() {
