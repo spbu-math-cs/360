9ff9ceeefb8e59ebe6bd435575b5727b047f2a05
mozhaa <mozhay2005@gmail.com>
2023-12-14 13:37:26 +0300

    fix: reset home.html

--- src/main/kotlin/com/ne_rabotaem/utils/Hashing.kt
+++ src/main/kotlin/com/ne_rabotaem/utils/Hashing.kt
@@ -3,12 +3,13 @@ package com.ne_rabotaem.utils
 import java.security.MessageDigest
 
 object Hashing {
+    @OptIn(ExperimentalStdlibApi::class)
     fun getHash(data: String): String {
         val digest = MessageDigest.getInstance("SHA-256").digest((data).toByteArray())
 
         return with(StringBuilder()) {
             digest.forEach {
-                b -> append(String.format("%x", b))
+                b -> append(b.toHexString())
             }
             toString().lowercase()
         }
